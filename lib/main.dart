3e00d327983d5cf515aa9af530eeaf2aba4393e3
A7mdlbanna <ahmedbanna200@gmail.com>
2023-11-04 20:12:46 +0200

    hotfix

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:bloc/bloc.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 import 'app.dart';
@@ -11,11 +10,7 @@ import 'firebase_options.dart';
 void main() async{
 
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  final fcmToken = await FirebaseMessaging.instance.getToken();
-  await FirebaseMessaging.instance.setAutoInitEnabled(true);
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   await CacheHelper.init();
   Bloc.observer = MyBlocObserver();
